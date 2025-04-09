4d62db86e5e4071e162ed250887a54c42fa96f54
Jonathan Weigele <jonathan.t.l.weigele@gmx.de>
2025-04-09 23:33:17 +0200

    char creator erstellt, gehört eig zu #1

--- charcreation.py
+++ charcreation.py
@@ -1,2 +1,11 @@
+import datetime
 class charakter:
-    def __init__(self, bwid, vorname, nachname, nickname, gender, geb, personality, )
+    def __init__(self, bwid, vorname, nachname, nickname, gender, geb, sexuality, personality):
+        self.bwid = int(bwid)
+        self.vorname = str(vorname)
+        self.nachname = str(nachname)
+        self.nickname = str(nickname)
+        self.gender = str(gender)
+        self.geb = datetime.datetime(YYYY, MM, DD)
+        self.sexuality = str(sexuality)
+        self.personality = str(personality)
